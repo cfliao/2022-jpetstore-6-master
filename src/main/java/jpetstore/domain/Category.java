9945ab7b10cb5b114a70340768050b5d48e0c37f
Chun-Feng Liao <cfliao@nccu.edu.tw>
2022-03-01 23:42:07 +0800

    Add Lombok

--- src/main/java/jpetstore/domain/Category.java
+++ src/main/java/jpetstore/domain/Category.java
@@ -1,10 +1,17 @@
 package jpetstore.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@ToString
 @Entity
 @Table(name = "category")
 public class Category
@@ -19,36 +26,36 @@ public class Category
 	@Column(name = "descn")
 	private String description;
 
-	public String getCategoryId() {
-		return categoryId;
-	}
-
-	public void setCategoryId(String categoryId) {
-		this.categoryId = categoryId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	@Override
-	public String toString() {
-		return "Category{" +
-				"categoryId='" + categoryId + '\'' +
-				", name='" + name + '\'' +
-				", description='" + description + '\'' +
-				'}';
-	}
+//	public String getCategoryId() {
+//		return categoryId;
+//	}
+//
+//	public void setCategoryId(String categoryId) {
+//		this.categoryId = categoryId;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getDescription() {
+//		return description;
+//	}
+//
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "Category{" +
+//				"categoryId='" + categoryId + '\'' +
+//				", name='" + name + '\'' +
+//				", description='" + description + '\'' +
+//				'}';
+//	}
 }
